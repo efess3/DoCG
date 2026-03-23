eab56128c5baed5c9cc89251d5fca2c8eb19f96c
Mateusz Janeczek <m.janeczek777@gmail.com>
2026-03-23 19:22:23 +0100

    player animation

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -6,30 +6,48 @@ public class PlayerMovement : MonoBehaviour
 
     private Rigidbody2D rb;
     private Vector2 movement;
+    private Animator animator;
+    private SpriteRenderer spriteRenderer;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     void Update()
     {
+        if (movement.x > 0)
+        {
+            spriteRenderer.flipX = false; // patrzy w prawo
+        }
+        else if (movement.x < 0)
+        {
+            spriteRenderer.flipX = true; // patrzy w lewo
+        }
+        
         if (GameManager.instance != null && !GameManager.instance.isGameActive)
         {
             movement = Vector2.zero;
+            animator.SetBool("isRunning", false);
             return;
         }
 
-        // odczyt inputu
+        // input
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
         movement = movement.normalized;
+
+        bool isMoving = movement.magnitude > 0;
+        Debug.Log("isRunning: " + (movement.magnitude > 0));
+        animator.SetBool("isRunning", isMoving);
+
     }
 
     void FixedUpdate()
     {
         Vector2 newPos = rb.position + movement * moveSpeed * Time.fixedDeltaTime;
-
         rb.MovePosition(newPos);
     }
 
--- Assets/Scripts/Systems/Health.cs
+++ Assets/Scripts/Systems/Health.cs
@@ -1,18 +1,22 @@
 using UnityEngine;
 
-
 public class Health : MonoBehaviour
 {
     public int maxHealth = 10;
     public int currentHealth;
+    private Animator animator;
+    private bool isDead = false;
 
     void Start()
     {
         currentHealth = maxHealth;
+        animator = GetComponent<Animator>();
     }
 
     public void TakeDamage(int damage)
     {
+        if (isDead) return;
+
         currentHealth -= damage;
 
         if (currentHealth <= 0)
@@ -23,12 +27,18 @@ public class Health : MonoBehaviour
 
     void Die()
     {
-        Destroy(gameObject);
+        isDead = true;
+
+        animator.SetTrigger("Death");
+
+        GetComponent<Collider2D>().enabled = false;
+
+        Destroy(gameObject, 1f);
     }
 
     public void IncreaseMaxHealth(int amount)
     {
         maxHealth += amount;
-        currentHealth += amount; // Doleczenie postaci o zdobytą wartość maksymalnego zdrowia
+        currentHealth += amount;
     }
 }
