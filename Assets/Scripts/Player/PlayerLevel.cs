214d646b7d6b83f6e8db18079d5954540eb4aaeb
Mateusz Janeczek <m.janeczek777@gmail.com>
2026-03-24 21:22:12 +0100

    exp bar fix

--- Assets/Scripts/Player/PlayerLevel.cs
+++ Assets/Scripts/Player/PlayerLevel.cs
@@ -4,55 +4,55 @@ using UnityEngine.UI;
 public class PlayerLevel : MonoBehaviour
 {
     public int level = 1;
-
     public int currentXP = 0;
-
     public int xpToNextLevel = 10;
-
-    public Slider slider;
-
     private Animator animator;
 
     public void Start()
     {
         animator = GetComponent<Animator>();
-        if (HealthSystem.Instance != null)
-        {
-
-            HealthSystem.Instance.maxManaPoint = xpToNextLevel;
-            HealthSystem.Instance.manaPoint = currentXP;
-            
-            HealthSystem.Instance.UpdateGraphics();
-        }
+        SyncXPWithUI();
     }
+
     public void AddXP(int amount)
     {
         currentXP += amount;
-        HealthSystem.Instance.RestoreMana(amount);
+        
+        if (HealthSystem.Instance != null)
+        {
+            HealthSystem.Instance.RestoreMana(amount);
+        }
 
         if (currentXP >= xpToNextLevel)
         {
             LevelUp();
-            HealthSystem.Instance.manaPoint = 0f;
-            HealthSystem.Instance.UpdateGraphics();
         }
     }
 
     void LevelUp()
     {
         level++;
-
         currentXP -= xpToNextLevel;
-
+        
         xpToNextLevel = Mathf.RoundToInt(xpToNextLevel * 1.1f);
-
-        slider.maxValue = xpToNextLevel;
+        SyncXPWithUI();
 
         Debug.Log("LEVEL UP: " + level);
 
         if (UpgradeManager.instance != null)
         {
             UpgradeManager.instance.ShowUpgrades();
+        }
+    }
+
+    // Nowa metoda pomocnicza, która "mówi" wszystkim paskom, jakie są nowe limity
+    private void SyncXPWithUI()
+    {
+        if (HealthSystem.Instance != null)
+        {
+            HealthSystem.Instance.maxManaPoint = xpToNextLevel;
+            HealthSystem.Instance.manaPoint = currentXP;
+            HealthSystem.Instance.UpdateGraphics(); 
         }
     }
 }
