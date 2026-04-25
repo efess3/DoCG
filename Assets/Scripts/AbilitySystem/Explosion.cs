622fc9b1b859e6ae7529f4a5e8248e990026eb52
Mateusz Janeczek <m.janeczek777@gmail.com>
2026-04-25 13:50:04 +0200

    skill2 collision fix

--- Assets/Scripts/AbilitySystem/Explosion.cs
+++ Assets/Scripts/AbilitySystem/Explosion.cs
@@ -4,6 +4,22 @@ public class Explosion : MonoBehaviour
 {
     public GameObject[] explosions;
 
+    void Start()
+    {
+        // Na starcie wyłączamy collider
+        Collider2D col = GetComponent<Collider2D>();
+        if (col != null) col.enabled = false;
+
+        // Uruchamiamy odliczanie
+        StartCoroutine(ActivateColliderAfterDelay(0.7f));
+    }
+
+    IEnumerator ActivateColliderAfterDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        GetComponent<Collider2D>().enabled = true;
+    }
+
     public void TriggerExplosions()
     {
         StartCoroutine(ExplodeSequence());
